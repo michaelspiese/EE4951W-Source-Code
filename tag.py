993dfb3e797b41ad0870277127c2ed46b86135a5
Michael Spiese <spies046@umn.edu>
2023-03-08 17:40:02 -0600

    rough positional calculations added
    using distance data from two anchors to calculate 2D position in space

--- tag.py
+++ tag.py
@@ -1,55 +1,53 @@
 import serial
 import time
+import math
+from statistics import mean
 
-# try:
-#     ser = serial.Serial("COM7",115200,8,'N',1,100,0)
-#     ser.write(b'\r\r')
-#     time.sleep(1)
-#     ser.write(b'lec\r')
-#     data = b''
-#     while data == b'':
-#         data = ser.readline()
-#         print(data)
-#     ser.close()
-# except Exception as e:
-#     print(Exception)
-#     exit()
-
-# ser.open()
-
-# for i in range(20):
-#     temp = 0
-#     while temp == 0:
-#         data = ser.readline()
-#         if data != b'':
-#             print(data.decode(),end='')
-#             temp = 1
-
-# ser.close()
-
-DWM = serial.Serial(port="COM7", baudrate=115200)
+DWM = serial.Serial(port="COM3", baudrate=115200)
+precision = 5
 
 DWM.write("\r\r".encode())
 time.sleep(1)
 DWM.write("lec\r".encode())
 time.sleep(1)
 
+devices = {"DD1D": [0 for n in range(precision)], "CC83": [0 for n in range(precision)]}
+
+i = 0
 while True:
     try:
         line = DWM.readline()
         if(line):
-            if len(line) >= 20:
+            if len(line) >= 60:
                 parse = line.decode().split(',')
-                pos = parse[-1].strip()
-                print(pos)
-            # else:
-            #     print("Position not calculated: ", line.decode().strip())
+                try:
+                    devices[parse[3]][i%precision] = float(parse[7])
+                    devices[parse[9]][i%precision] = float(parse[13].strip())
+                except:
+                    devices["CC83"][i%precision] = 0
+                    devices["DD1D"][i%precision] = 0
+                i += 1
     except Exception as ex:
         print(ex)
         break
     except KeyboardInterrupt:
         print("INTERRUPT")
         break
+
+    distI = mean(devices["DD1D"])
+    distA = mean(devices["CC83"])
+
+    try:
+        angI = math.acos((distI**2 + 2.1**2 - distA**2) / (2 * distI * 2.1))
+        angA = math.acos((distA**2 + 2.1**2 - distI**2) / (2 * distA * 2.1))
+    except:
+        angI = 0.000
+        angA = 0.000
+
+    x = distI * math.cos(angI)
+    y = distI * math.sin(angI)
+
+    print(f"magI:{distI:.3f}  ", f"magA:{distA:.3f}      ", f"angI:{math.degrees(angI):.3f}  ", f"angA:{math.degrees(angA):.3f}      ", f"x:{x:.3f}  ", f"y:{y:.3f}")
 
 DWM.write("lec\r".encode())
 DWM.close()
