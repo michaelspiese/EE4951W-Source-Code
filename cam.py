636789591311da8825be4bf602544e627a2189d9
michaelspiese <michael.spiese@gmail.com>
2023-04-13 10:32:18 -0500

    pushing new work from initial focus testing

--- cam.py
+++ cam.py
@@ -2,18 +2,25 @@ from picamera2 import Picamera2, Preview
 from libcamera import Transform
 import time
 
+alp = "abcdefghijklmnopqrstuvwxyz"
+
 class ArducamHawkEye:
 	def __init__(self, res):
 		self.pIdx = 0;
 		self.cam = Picamera2()
 		self.config = self.cam.create_preview_configuration(main={"size": res},transform=Transform(hflip=True, vflip=True))
 		self.cam.configure(self.config)
-		#self.cam.start_preview(Preview.QT)
+		# self.cam.start_preview(Preview.QT)
 		self.cam.start()
+		self.set_focus(6.5)
 
 	def takePhoto(self):
-		self.cam.capture_file(f"{self.pIdx}.jpg")
+		#self.cam.capture_file(f"/home/ee4951w/Pictures/{alp[self.pIdx%26]}.jpg")
+		self.cam.capture_file(f"/home/ee4951w/Pictures/{self.pIdx}.jpg")
 		self.pIdx+=1
+		
+	def set_focus(self, focus):
+		self.cam.set_controls({"AfMode": 0, "LensPosition": focus})
 					
 if __name__ == "__main__":
 	c = ArducamHawkEye((1920,1080))
